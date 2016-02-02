b60013033f377e82c455756633cd68906d2c7440
codeamac02 <codeamac02@gmail.com>
2016-02-02 17:38:03 -0600

    finished anagrams

--- semana 1/lunes/sinatra_skeleton_anagrams/app/controllers/index.rb
+++ semana 1/lunes/sinatra_skeleton_anagrams/app/controllers/index.rb
@@ -2,9 +2,28 @@ get '/' do
   erb :index
 end
 
+get '/model' do
+  erb :my_model
+end
+
+get '/list' do
+  @models = Model.all
+  erb :model_list
+end
+
+post '/submit' do
+	@model = Model.new(params[:model])
+	if @model.save
+		redirect '/list'
+	else
+		"Sorry, there was an error!"
+	end
+end
+
 get '/:new_word' do
 	@new_word = params[:new_word]
-
+  canon = get_canonical(@new_word)
+  @words = Word.where(canonical:canon)
   erb :word
 end
 
--- semana 1/lunes/sinatra_skeleton_anagrams/db/seeds.rb
+++ semana 1/lunes/sinatra_skeleton_anagrams/db/seeds.rb
@@ -1,2 +1,35 @@
-# Este archivo sirve para correr código que te permita 
-# rellenar tu base de datos con información. 
+# Este archivo sirve para correr código que te permita
+# rellenar tu base de datos con información.
+
+# animal_list = [ "Rat",
+#     "Dog",
+#     "Pig",
+#     "Goat"]
+#
+# animal_list.each do |name|
+#   Model.create( name: name )
+# end
+
+def load_words
+  words = []
+  File.open('words.txt') do |f|
+    while line = f.gets
+      words.push(line.chomp)
+    end
+  end
+  return words
+end
+
+def get_canonical(word)
+  w = word.downcase.split('')
+  w.sort!
+  canon = w.join
+  return canon
+end
+
+words = load_words
+
+for word in words do
+  canon = get_canonical(word)
+  Word.create(name:word, canonical:canon)
+end
