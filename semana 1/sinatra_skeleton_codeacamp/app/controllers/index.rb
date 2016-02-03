4298f32de907c9ededd42f60b5fb9e82b7018aa0
codeamac02 <codeamac02@gmail.com>
2016-02-03 15:36:00 -0600

    cool_url forms

--- semana 1/sinatra_skeleton_codeacamp/app/controllers/index.rb
+++ semana 1/sinatra_skeleton_codeacamp/app/controllers/index.rb
@@ -1,5 +1,19 @@
 get '/' do
-  # La siguiente linea hace render de la vista 
-  # que esta en app/views/index.erb
-  erb :index
+  puts "Este es un GET a (/)"
+  puts "Esto es lo que vale Params: #{params.inspect}"
+  erb :index, :layout => :layout # Esto renderea una vista llamada index.erb
+end
+
+get '/cool_url' do
+  puts "Este es un GET a /cool_url"
+  puts "Esto es lo que vale Params: #{params.inspect}"
+  @message = params[:message]
+  erb :get_cool_url  # Esto renderea una vista llamada get_cool_url.erb
+end
+
+post '/cool_url' do
+  puts "Este es un POST a /cool_url"
+  puts "Esto es lo que vale Params: #{params.inspect}"
+  @user = params[:user]
+  erb :post_cool_url  # Esto renderea una vista llamada post_cool_url.erb
 end
