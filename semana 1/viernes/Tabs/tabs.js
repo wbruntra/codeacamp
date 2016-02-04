665dd450bd1fbd0a1aa73131e6150fbf26c15ea0
William Bruntrager <w.bruntrager@gmail.com>
2016-02-04 14:04:31 -0600

    tabs project complete

--- semana 1/viernes/Tabs/tabs.js
+++ semana 1/viernes/Tabs/tabs.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
   // Put your JavaScript behavior here
+  
+  $('#tab1').show();
+  
+  $('a').click(function() {
+    $('.tab').hide();
+    $('li').removeClass('active');
+    tabNo = $(this).attr('href');
+    console.log(tabNo);
+    $(this).parent().addClass('active');
+    $(tabNo).show();
+  });
 });
