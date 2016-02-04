119596bdd7f667bc73c05a1b5b7df650dccac1ac
William Bruntrager <w.bruntrager@gmail.com>
2016-02-04 13:46:32 -0600

    fixed topbar nav exercise

--- semana 1/jueves/fixed_navigation_files_dup/navbar.js
+++ semana 1/jueves/fixed_navigation_files_dup/navbar.js
@@ -1,4 +1,16 @@
+didScroll = true;
+
 $(window).on("scroll", function () {
   // Aquí deberás escribir la lógica que modificará la barra
-  
+  didScroll = true;
 });
+
+setInterval(function () {
+  if (didScroll) {
+    didScroll = false;
+    var sPos = $(window).scrollTop();
+    console.log(sPos);
+    var opacityLevel = Math.max(.8, 1-sPos/1000);
+    $('.top-nav').css('opacity',opacityLevel);
+  }
+}, 250);
