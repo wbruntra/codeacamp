1909a15fc78ddfab1d954fcfe15e5239e484c8d0
William Bruntrager <w.bruntrager@gmail.com>
2016-02-09 18:48:15 -0600

    finished dado async

--- semana 3/lunes/El_Dado/app/controllers/index.rb
+++ semana 3/lunes/El_Dado/app/controllers/index.rb
@@ -4,14 +4,14 @@ get '/' do
 end
 
 
-# QUE HACER?: Convierte esta ruta para que utilice Ajax y si la petición no es de Ajax de igual forma sirva. 
+# QUE HACER?: Convierte esta ruta para que utilice Ajax y si la petición no es de Ajax de igual forma sirva.
 post '/rolls' do
-	# Si el user nos pasa un valor, lo usamos y so no creamos uno. 
+	# Si el user nos pasa un valor, lo usamos y so no creamos uno.
 
   value = params[:value] ? params[:value].to_i : nil
 
   @roll = value ? Roll.create({ value: value }) : Roll.create
 
-  erb :index  # TIP: Que esta haciendo esto y que debe hacer diferente. 
+  erb :_die_roll, layout: false # TIP: Que esta haciendo esto y que debe hacer diferente. 
 
 end
--- semana 3/lunes/El_Dado/public/js/application.js
+++ semana 3/lunes/El_Dado/public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-	// Este código corre despues de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
+	// Este código corre despues de que `document` fue cargado(loaded)
+	// completamente.
+	// Esto garantiza que si amarramos(bind) una función a un elemento
 	// de HTML este exista ya en la página.
-  	
+  $('form').submit(function(e) {
+    e.preventDefault();
+    get_roll();
+  });
 });
+
+function get_roll() {
+  $.ajax({
+     type: "post",
+     url: "/rolls",
+     dataType: "html",
+     success: loadRoll
+     });
+}
+
+function loadRoll(response) {
+  $('#die').html(response);
+}
